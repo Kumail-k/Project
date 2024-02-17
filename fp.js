742763f47e10d06248f317aa353a3a907297198c
Kumail-k <160223070+Kumail-k@users.noreply.github.com>
2024-02-17 22:46:37 +0500

    Add files via upload

---
+++ fp.js
@@ -0,0 +1,35 @@
+alert("WELCOME CUSTOMER")
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('#navbar');
+let sections = document.querySelectorAll('.section');
+let navlinks = document.querySelectorAll('.header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navlinks.forEach(links =>{
+                links.classList.remove('ative');
+                document.querySelector("header nav a[href*='" + id + "']").classList.add("active");
+        
+                })    }
+    })
+}
+
+
+
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const menuIcon = document.querySelector('#menu-icon');
+    const navbar = document.querySelector('#navbar');
+  
+    menuIcon.addEventListener('click', function() {
+      menuIcon.classList.toggle('bx-x');
+      navbar.classList.toggle('active');
+    });
+  });
